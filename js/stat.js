e9fb9ae3720d4c29d94eda3aa494110f5c8abf83
Alex <lzlin@mail.ru>
2020-02-02 14:25:19 +1000

    добавляет функцию, создает текст на поле

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,34 @@
 'use strict';
 
-window.renderStatistics = function (ctx) {
-  ctx.fillStyle = 'rgba(19,9,51,0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+var SHADOW_WIDTH = 420;
+var SHADOW_HEIGHT = 270;
 
+var renderCloudShadow = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, SHADOW_WIDTH, SHADOW_HEIGHT);
+};
+
+var renderCloud = function(ctx, stroke, x, y, color) {
   ctx.beginPath();
-  ctx.strokeStyle = '#130933';
-  ctx.moveTo(100, 10);
+  ctx.strokeStyle = stroke;
+  ctx.moveTo(x, y);
   ctx.lineTo(310, 20);
   ctx.lineTo(520, 10);
   ctx.lineTo(520, 280);
   ctx.lineTo(100, 280);
   ctx.lineTo(100, 10);
   ctx.stroke();
-  ctx.fillStyle = '#fff';
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 50);
+  ctx.fillText('Список результатов: ', 120, 70);
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloudShadow(ctx, 110, 20, 'rgba(19,9,51,0.7)');
+  renderCloud(ctx, '#130933', 100, 10, '#fff');
 };
